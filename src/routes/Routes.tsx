57dbb6ab72da1d4d467868b59b9941d4290f6f76
fermin234 <fermin234@hotmail.com>
2023-10-13 15:04:26 -0300

    create routing

---
+++ src/routes/Routes.tsx
@@ -0,0 +1,37 @@
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import {RootStackParams} from '../types';
+import Home from '../views/Home';
+import Detail from '../views/Detail';
+
+const Stack = createNativeStackNavigator<RootStackParams>();
+
+const routesScreenDefaultOptions = {
+  headerStyle: {
+    backgroundColor: 'rgba(7,26,93,255)',
+  },
+  headerTitleStyle: {
+    color: 'white',
+  },
+};
+
+const Routes = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen
+          name="Home"
+          component={Home}
+          options={routesScreenDefaultOptions}
+        />
+        <Stack.Screen
+          name="Detail"
+          component={Detail}
+          options={routesScreenDefaultOptions}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default Routes;
